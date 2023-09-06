1f1490d2d6f424c982c3b451e6e351c4466304d4
langemittbacken <ludvig.hedlund@hotmail.se>
2023-09-06 09:48:45 +0000

    add tests for bucket and routingTable

---
+++ kademlia/bucket_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBucket(t *testing.T) {
+	bucket := newBucket()
+	mainContact := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	bucket.AddContact(mainContact)
+	//try to overload a bucket
+	for i := 0; i < bucketSize+5; i++ {
+		bucket.AddContact(NewContact(NewRandomKademliaID(), "localhost:8001"))
+	}
+	if bucket.Len() > bucketSize {
+		t.Errorf("bucket was bigger (%d) than max allowed (%d)", bucket.Len(), bucketSize)
+	}
+}
--- kademlia/routingtable_test.go
+++ kademlia/routingtable_test.go
@@ -8,15 +8,35 @@ import (
 func TestRoutingTable(t *testing.T) {
 	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
 
-	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	furthest := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001")
+	rt.AddContact(furthest)
 	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
 	rt.AddContact(NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
 	rt.AddContact(NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
 	rt.AddContact(NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002"))
+	closest := NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002")
+	rt.AddContact(closest)
 
 	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), 20)
 	for i := range contacts {
 		fmt.Println(contacts[i].String())
+	}
+
+	closestBucketIndex := rt.getBucketIndex(closest.ID)
+	furthestBucketIndex := rt.getBucketIndex(furthest.ID)
+
+	if closestBucketIndex != 0 {
+		t.Errorf("closest bucket index excpected [%d], got [%d]", 0, closestBucketIndex)
+	}
+
+	if furthestBucketIndex != len(rt.buckets)-1 {
+		t.Errorf("closest bucket index excpected [%d], got [%d]", len(rt.buckets)-1, furthestBucketIndex)
+	}
+
+	if contacts[0].String() != closest.String() {
+		t.Errorf("closest should be [%s] but was [%s]", closest.String(), contacts[0].String())
+	}
+	if contacts[len(contacts)-1].String() != furthest.String() {
+		t.Errorf("furthest should be [%s] but was [%s]", furthest.String(), contacts[len(contacts)-1].String())
 	}
 }
