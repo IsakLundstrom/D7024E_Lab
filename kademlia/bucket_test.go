4377cc60d3f1e6f8a826bba0b8ea7d3ba941a297
langemittbacken <ludvig.hedlund@hotmail.se>
2023-10-06 08:09:17 +0000

    small bucket test improvement

--- kademlia/bucket_test.go
+++ kademlia/bucket_test.go
@@ -8,6 +8,11 @@ func TestBucket(t *testing.T) {
 	bucket := newBucket()
 	mainContact := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
 	bucket.AddContact(mainContact)
+	bucket.AddContact(NewContact(NewKademliaID("EEEEEEEE00000000000000000000000000000000"), "localhost:8002"))
+	bucket.AddContact(mainContact)
+	if bucket.Len() == 3 {
+		t.Errorf("bucket was able to add a duplicate!")
+	}
 	//try to overload a bucket
 	for i := 0; i < bucketSize+5; i++ {
 		bucket.AddContact(NewContact(NewRandomKademliaID(), "localhost:8001"))
@@ -15,4 +20,5 @@ func TestBucket(t *testing.T) {
 	if bucket.Len() > bucketSize {
 		t.Errorf("bucket was bigger (%d) than max allowed (%d)", bucket.Len(), bucketSize)
 	}
+
 }
